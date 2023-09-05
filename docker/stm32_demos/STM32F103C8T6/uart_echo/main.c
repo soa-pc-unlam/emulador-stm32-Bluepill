d59ea4736f5f2c4ee6f4f95358a134e1e81c368c
soa-pc-unlam <soaunlam2021@gmail.com>
2023-09-05 14:34:37 +0000

    se realizan cambios en los ejemplos

---
+++ docker/stm32_demos/STM32F103C8T6/uart_echo/main.c
@@ -0,0 +1,30 @@
+#define USE_STDPERIPH_DRIVER
+#include "stm32f10x.h"
+
+int main(void)
+{
+    uint8_t b;
+
+    init_led();
+
+    init_rs232();
+    USART_Cmd(USART1, ENABLE);
+
+    while(1) {  
+	    send_byte('A');
+    	/* Loop until the USART2 has received a byte. */
+        while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
+	    send_byte('D');
+
+        /* Capture the received byte and print it out. */
+        b = (USART_ReceiveData(USART1) & 0x7F);
+        send_byte('G');
+        send_byte('o');
+        send_byte('t');
+        send_byte(':');
+        send_byte(b);
+        send_byte('\r');
+        send_byte('\n');
+    }
+return 0;    
+}	
